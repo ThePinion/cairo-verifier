5741588ba4ceb181cf90b7c03a1d1ebbf056638f
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-02-07 14:02:01 +0000

    cleanup

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{stdin, Read};
 
+use cairo_args_runner::{Arg, Felt252, VecFelt252};
 use clap::Parser;
 
 use crate::{ast::Exprs, json_parser::ProofJSON, stark_proof::StarkProof};
@@ -12,44 +13,36 @@ mod stark_proof;
 mod utils;
 mod builtins;
 
-// lalrpop_mod!(pub parser);
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+enum Cli {
+    Parse,
+    Verify {
+        /// Path to compiled sierra file
+        target: String,
+    },
+}
 
 fn main() -> anyhow::Result<()> {
+    match Cli::parse() {
+        Cli::Parse => println!("{}", parse()?),
+        Cli::Verify { target } => println!("{:?}", run(parse()?, target)?)
+    }
+    Ok(())
+}
+
+fn parse() -> anyhow::Result<String> {
     let mut input = String::new();
     stdin().read_to_string(&mut input)?;
     let proof_json = serde_json::from_str::<ProofJSON>(&input)?;
     let stark_proof = StarkProof::try_from(proof_json)?;
     let exprs = Exprs::from(stark_proof);
-    let result = exprs.to_string();
-
-    println!("{result:?}");
-    Ok(())
+    Ok(exprs.to_string())
 }
 
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    /// Path to compiled sierra file
-    target: String,
+fn run(parsed: String, target: String) -> anyhow::Result<Vec<Felt252>> {
+        let target = target;
+        let function = "main";
+        let args: VecFelt252 = serde_json::from_str(&parsed).unwrap();
+        Ok(cairo_args_runner::run(&target, function, &[Arg::Array(args.to_vec())])?)
 }
-
-// #[allow(dead_code)]
-// fn old_main() -> anyhow::Result<()> {
-//     let cli = Cli::parse();
-//     let mut input = String::new();
-//     stdin().read_to_string(&mut input)?;
-
-//     let parsed = parser::CairoParserOutputParser::new()
-//         .parse(&input)
-//         .map_err(|e| anyhow::anyhow!("{}", e))?;
-//     let result = parsed.to_string();
-
-//     let target = cli.target;
-//     let function = "main";
-//     let args: VecFelt252 = serde_json::from_str(&result).unwrap();
-
-//     let result = run(&target, function, &[Arg::Array(args.to_vec())])?;
-
-//     println!("{result:?}");
-//     Ok(())
-// }
